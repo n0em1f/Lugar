b11ee1e42c5235a398c50e044d9c0d4cfcb66c6c
Noemi <noemi.feceu@gmail.com>
2023-07-18 11:22:42 +0300

    first commit - commiting all files to the repository

---
+++ main.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector("nav");
+const mobileNav = document.querySelector("nav.mobile-nav");
+
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".mobile-menu-container .close-icon");
+const mobileMenuContainer = document.querySelector(".mobile-menu-container");
+
+menuIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () => {
+    mobileMenuContainer.classList.remove("active");
+});
